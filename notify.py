253a12ea5d500a022384516644b29711e1a3037e
Abhishek kumar <72270897+softhackpro@users.noreply.github.com>
2021-01-10 06:51:22 +0000

    Add files via upload

---
+++ notify.py
@@ -0,0 +1,28 @@
+# This notification app made by Abhishek Kumar
+
+# This is english to hindi vocabulary notification app. It give 
+# nofication every 10 min.
+
+# It is open source app anyone change it and use it. 
+
+
+import time 
+import schedule
+import random
+from plyer import notification 
+
+def Notification(): 
+	file = open("notify/words.txt").readlines()
+	word = random.choice(file)
+	notification.notify( 
+			title = "English Vocabulary", 
+			message = word,
+			#app_icon = "/home/abhishek/Desktop/notif.jpeg",
+			timeout = 15
+					) 
+schedule.every(10).minutes.do(Notification)
+#schedule.every().hour.do(notification)
+
+while True:
+	schedule.run_pending()
+	time.sleep(1)
